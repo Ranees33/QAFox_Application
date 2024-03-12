6a75ea6a74ca8e3fdec767e2f9aac0ee47f2334b
Ranees33 <raneesdeveloper@gmail.com>
2024-03-12 20:25:38 +0530

    Added two page objects and two test methods for login and register test cases, and there are some changes made in test_tabletproduct and test_product_screenshot

---
+++ pageObjects/registerPage.py
@@ -0,0 +1,72 @@
+from selenium.webdriver.common.by import By
+
+from configProperties.configinfo import Testdata
+from pageObjects.basePage import BasePage
+
+
+class RegisterPage(BasePage):
+    Click_MyAccount = (By.XPATH, "//span[text()='My Account']")
+    Click_Register = (By.LINK_TEXT, "Register")
+    Enter_Firstname = (By.ID, "input-firstname")
+    Enter_Lastname = (By.ID, "input-lastname")
+    Enter_Email = (By.ID, "input-email")
+    Enter_Phonenumber = (By.ID, "input-telephone")
+    Enter_Password = (By.ID, "input-password")
+    Enter_Password_Confirm = (By.ID, "input-confirm")
+    Select_Privacypolicy = (By.NAME, "agree")
+    Click_Submit_Btn = (By.XPATH, "//input[@type='submit']")
+    Click_Continue_Btn = (By.LINK_TEXT, "Continue")
+    Get_Email_AlreadyReg_Text = (By.XPATH, "//*[@id='account-register']/div[1]")
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver.get(Testdata.App_Url)
+
+    def do_click_myaccount(self):
+        self.do_click(self.Click_MyAccount)
+
+    def do_click_register(self):
+        self.do_click(self.Click_Register)
+
+    def do_enter_firstname(self, text):
+        self.do_send_keys(self.Enter_Firstname, text)
+
+    def do_enter_lastname(self, text):
+        self.do_send_keys(self.Enter_Lastname, text)
+
+    def do_enter_email(self, text):
+        self.do_send_keys(self.Enter_Email, text)
+
+    def do_enter_phone_number(self, text):
+        self.do_send_keys(self.Enter_Phonenumber, text)
+
+    def do_enter_password(self, text):
+        self.do_send_keys(self.Enter_Password, text)
+
+    def do_enter_password_for_confirm(self, text):
+        self.do_send_keys(self.Enter_Password_Confirm, text)
+
+    def do_select_privacy(self):
+        self.do_click(self.Select_Privacypolicy)
+
+    def do_click_submit_btn(self):
+        self.do_click(self.Click_Submit_Btn)
+
+    # def do_click_continue_btn(self):
+    #     self.do_click(self.Click_Continue_Btn)
+    #
+    # def get_account_created_success_url(self):
+    #     account_creation_URL = self.get_current_url(self)
+    #     print("The Url for created a new account is : " + account_creation_URL)
+
+    def get_email_already_registrd_text(self):
+        self.get_element_text(self.Get_Email_AlreadyReg_Text)
+        # actual_Text = self.get_element_text(self.Get_Email_AlreadyReg_Text)
+        # expected_Text = "Warning: E-Mail Address is already registered!"
+        # assert actual_Text == expected_Text, f"Expected: {expected_Text}, Actual: {actual_Text}"
+        # print("This Email Address Already Registered Text is : " +
+        #       self.get_element_text(self.Get_Email_AlreadyReg_Text))
+
+
+
+
---
+++ tests/test_register.py
@@ -0,0 +1,31 @@
+import time
+
+import pytest
+
+from pageObjects.registerPage import RegisterPage
+
+
+@pytest.mark.usefixtures("setup_and_teardown")
+class TestRegister:
+    def test_register(self):
+        to_create_an_account = RegisterPage(self.driver)
+        to_create_an_account.do_click_myaccount()
+        to_create_an_account.do_click_register()
+        to_create_an_account.do_enter_firstname("Tony")
+        to_create_an_account.do_enter_lastname("Boniee")
+        to_create_an_account.do_enter_email("tonyboniee@gmail.com")
+        to_create_an_account.do_enter_phone_number("0123456789")
+        to_create_an_account.do_enter_password("tonyboniee")
+        to_create_an_account.do_enter_password_for_confirm("tonyboniee")
+        to_create_an_account.do_select_privacy()
+        to_create_an_account.do_click_submit_btn()
+        # to_create_an_account.do_click_continue_btn()
+        # to_create_an_account.get_account_created_success_url()
+        time.sleep(10)
+        actual_Text = to_create_an_account.get_email_already_registrd_text()
+        expected_Text = "Warning: E-Mail Address is already registered!"
+        assert actual_Text == expected_Text, f"Expected: {expected_Text}, Actual: {actual_Text}"
+        print("This Email Address Already Registered Text is : " +
+              to_create_an_account.get_email_already_registrd_text())
+
+
